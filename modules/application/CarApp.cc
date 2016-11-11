47338487ada496ca29492e02fdd0936ae0085e99
Vladimir Matena <matena@d3s.mff.cuni.cz>
2016-11-11 19:01:30 +0100

    Remove demo app, add empty one

---
+++ modules/application/CarApp.cc
@@ -0,0 +1,35 @@
+#include "modules/application/CarApp.h"
+
+using Veins::TraCIMobilityAccess;
+using Veins::AnnotationManagerAccess;
+
+Define_Module(CarApp);
+
+void CarApp::initialize(int stage) {
+	BaseWaveApplLayer::initialize(stage);
+	if (stage == 0) {
+		mobility = TraCIMobilityAccess().get(getParentModule());
+		traci = mobility->getCommandInterface();
+		traciVehicle = mobility->getVehicleCommandInterface();
+		annotations = AnnotationManagerAccess().getIfExists();
+		ASSERT(annotations);
+	}
+}
+
+void CarApp::onBeacon(WaveShortMessage* wsm) {
+}
+
+void CarApp::onData(WaveShortMessage* wsm) {
+}
+
+void CarApp::sendMessage(std::string blockedRoadId) {
+}
+void CarApp::receiveSignal(cComponent* source, simsignal_t signalID, cObject* obj, cObject* details) {
+}
+void CarApp::handleParkingUpdate(cObject* obj) {
+}
+
+void CarApp::handlePositionUpdate(cObject* obj) {
+}
+void CarApp::sendWSM(WaveShortMessage* wsm) {
+}
---
+++ modules/application/CarApp.h
@@ -0,0 +1,31 @@
+#ifndef CARAPP_H
+#define CARAPP_H
+
+#include "veins/modules/application/ieee80211p/BaseWaveApplLayer.h"
+#include "veins/modules/mobility/traci/TraCIMobility.h"
+#include "veins/modules/mobility/traci/TraCICommandInterface.h"
+
+using Veins::TraCIMobility;
+using Veins::TraCICommandInterface;
+using Veins::AnnotationManager;
+
+class CarApp : public BaseWaveApplLayer {
+	public:
+		virtual void initialize(int stage);
+		virtual void receiveSignal(cComponent* source, simsignal_t signalID, cObject* obj, cObject* details);
+	protected:
+		TraCIMobility* mobility;
+		TraCICommandInterface* traci;
+		TraCICommandInterface::Vehicle* traciVehicle;
+		AnnotationManager* annotations;
+		simtime_t lastDroveAt;
+	protected:
+		virtual void onBeacon(WaveShortMessage* wsm);
+		virtual void onData(WaveShortMessage* wsm);
+		void sendMessage(std::string blockedRoadId);
+		virtual void handlePositionUpdate(cObject* obj);
+		virtual void handleParkingUpdate(cObject* obj);
+		virtual void sendWSM(WaveShortMessage* wsm);
+};
+
+#endif
