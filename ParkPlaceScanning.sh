7b3c5cf9b8f53e0c5714cd952457bb4e1b7b50d5
Vladimír Matěna <matena@d3s.mff.cuni.cz>
2016-12-05 18:19:10 +0100

    Make Car app build

---
+++ ParkPlaceScanning.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./ParkingPlaceScanning -l../veins-lte-master/simulte/src/lte -l../veins-lte-master/inet/src/inet -l../veins-lte-master/veins/src/veins -n"../veins-lte-master/simulte/src;../veins-lte-master/inet/src;../veins-lte-master/veins/src/veins;."
