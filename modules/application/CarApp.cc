ad9741518daf2ba0171a6f93e000aabe13c0f5be
Vladimír Matěna <matena@d3s.mff.cuni.cz>
2017-01-05 16:48:40 +0100

    Use logic from SimpleApp

--- modules/application/CarApp.cc
+++ modules/application/CarApp.cc
@@ -1,42 +1,70 @@
 #include "modules/application/CarApp.h"
 
-using Veins::TraCIMobilityAccess;
-using Veins::AnnotationManagerAccess;
-
 Define_Module(CarApp);
 
 void CarApp::initialize(int stage) {
-	BaseWaveApplLayer::initialize(stage);
 	if (stage == 0) {
-		mobility = TraCIMobilityAccess().get(getParentModule());
-		traci = mobility->getCommandInterface();
-		traciVehicle = mobility->getVehicleCommandInterface();
-		annotations = AnnotationManagerAccess().getIfExists();
-		ASSERT(annotations);
+		toDecisionMaker = findGate("toDecisionMaker");
+		fromDecisionMaker = findGate("fromDecisionMaker");
+		
+		mobility = dynamic_cast<Veins::TraCIMobility *>(getParentModule()->getSubmodule("veinsmobility"));
+		ASSERT(mobility);
+		
+		// Choose car mode
+		if (dblrand() < 0.1) {
+			mode = PARKING;
+		} else {
+			mode = NORMAL;
+		}
+		
+		// Schedule parking assistance request
+		scheduleAt(simTime() + uniform(0, 1), new cMessage("CallForAssistance"));
 	}
-	
-	// Choose car mode
-	if (dblrand() < 0.1) {
-		mode = PARKING;
+}
+
+void CarApp::handleMessage(cMessage *msg) {
+if (msg->isSelfMessage()) {
+		/*
+		 * Send a message to a random node in the network. Note that only the most necessary values
+		 * are set. Size of the message have to be set according to the real message (aka your used
+		 * .msg file). The values here are just a temporary placeholder.
+		 */
+		HeterogeneousMessage *testMessage = new HeterogeneousMessage();
+		testMessage->setNetworkType(DONTCARE);
+		testMessage->setName("Heterogeneous Test Message");
+		testMessage->setByteLength(10);
+
+		/* choose a random other car to send the message to */
+		Veins::TraCIScenarioManager* manager = Veins::TraCIScenarioManagerAccess().get();
+		std::map<std::string, cModule*> hosts = manager->getManagedHosts();
+		std::map<std::string, cModule*>::iterator it = hosts.begin();
+		std::advance(it, intrand(hosts.size()));
+		std::string destination("node[" + it->first + "]");
+		std::cout << "Sending message to " << destination << std::endl;
+		testMessage->setDestinationAddress(destination.c_str());
+
+		/* Finish the message and send it */
+		testMessage->setSourceAddress(mobility->getExternalId().c_str());
+		send(testMessage, toDecisionMaker);
+
+		/*
+		 * At 25% of the time send also a message to the main server. This message is sent via LTE
+		 * and is then simply handed to the decision maker.
+		 */
+		if(dblrand() < 0.25){
+			std::cout << "Sending message also to server" << std::endl;
+			HeterogeneousMessage* serverMessage = new HeterogeneousMessage();
+			serverMessage->setName("Server Message Test");
+			testMessage->setByteLength(10);
+			serverMessage->setNetworkType(LTE);
+			serverMessage->setDestinationAddress("server");
+			serverMessage->setSourceAddress(mobility->getExternalId().c_str());
+			send(serverMessage, toDecisionMaker);
+		}
+
+		scheduleAt(simTime() + 1, new cMessage("Send"));
 	} else {
-		mode = NORMAL;
+		HeterogeneousMessage *testMessage = dynamic_cast<HeterogeneousMessage *>(msg);
+		std::cout << "Received message " << msg->getFullName() << " from " << testMessage->getSourceAddress() << std::endl;
 	}
-}
-
-void CarApp::onBeacon(WaveShortMessage* wsm) {
-}
-
-void CarApp::onData(WaveShortMessage* wsm) {
-}
-
-void CarApp::sendMessage(std::string blockedRoadId) {
-}
-void CarApp::receiveSignal(cComponent* source, simsignal_t signalID, cObject* obj, cObject* details) {
-}
-void CarApp::handleParkingUpdate(cObject* obj) {
-}
-
-void CarApp::handlePositionUpdate(cObject* obj) {
-}
-void CarApp::sendWSM(WaveShortMessage* wsm) {
 }
--- modules/application/CarApp.h
+++ modules/application/CarApp.h
@@ -1,15 +1,12 @@
 #ifndef CARAPP_H
 #define CARAPP_H
 
-#include "veins/modules/application/ieee80211p/BaseWaveApplLayer.h"
+#include "veins/modules/heterogeneous/messages/HeterogeneousMessage_m.h"
+#include "veins/modules/mobility/traci/TraCIScenarioManager.h"
 #include "veins/modules/mobility/traci/TraCIMobility.h"
-#include "veins/modules/mobility/traci/TraCICommandInterface.h"
+#include "veins/base/utils/SimpleLogger.h"
 
-using Veins::TraCIMobility;
-using Veins::TraCICommandInterface;
-using Veins::AnnotationManager;
-
-/***
+/**
  * Car operation mode
  */
 enum CarMode {
@@ -17,26 +14,19 @@ enum CarMode {
 	PARKING /// Car is willing to park, will request assitance from others
 };
 
-class CarApp : public BaseWaveApplLayer {
-	public:
-		virtual void initialize(int stage);
-		virtual void receiveSignal(cComponent* source, simsignal_t signalID, cObject* obj, cObject* details);
+/**
+ * Car application module
+ */
+class CarApp : public cSimpleModule {
 	protected:
-		TraCIMobility* mobility;
-		TraCICommandInterface* traci;
-		TraCICommandInterface::Vehicle* traciVehicle;
-		AnnotationManager* annotations;
-		simtime_t lastDroveAt;
+		int toDecisionMaker;
+		int fromDecisionMaker;
+		Veins::TraCIMobility* mobility;
 	
 		CarMode mode = NORMAL;
-	
-	protected:
-		virtual void onBeacon(WaveShortMessage* wsm);
-		virtual void onData(WaveShortMessage* wsm);
-		void sendMessage(std::string blockedRoadId);
-		virtual void handlePositionUpdate(cObject* obj);
-		virtual void handleParkingUpdate(cObject* obj);
-		virtual void sendWSM(WaveShortMessage* wsm);
+
+		virtual void initialize(int stage);
+		virtual void handleMessage(cMessage *msg);
 };
 
 #endif
