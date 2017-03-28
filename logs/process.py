3fb36561d373fb4a22f22b57d5c2d4a7732bcd94
Vladimír Matěna <matena@d3s.mff.cuni.cz>
2017-03-28 23:41:35 +0200

    Update plotting script

---
+++ logs/process.py
@@ -0,0 +1,154 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_ete_latency():
+	data = []
+	times = []
+
+	with open("log.ete-lattency.txt") as f:
+		lines = f.readlines()
+
+		for line in lines:
+			parts = line.split(" ")
+			data.append(float(parts[2]) * 1000)
+			times.append(float(parts[0]))
+
+	ax1 = plt.subplot(121)
+	ax1.boxplot(data, notch=True)
+	plt.ylabel("latency in ms")
+	plt.title("End to end delay")
+
+	ax2 = plt.subplot(122)
+	ax2.plot(times, data, 'wo')
+	plt.xlabel("timestamp in seconds")
+	plt.ylabel("latency in milliseconds")
+	plt.title("End to end delay")
+
+	plt.savefig("EtE-latency.pdf")
+
+
+def plot_ete_distance():
+	data = []
+	times = []
+
+	with open("log.ete-distance.txt") as f:
+		lines = f.readlines()
+
+		for line in lines:
+			parts = line.split(" ")
+			data.append(float(parts[2]))
+			times.append(float(parts[0]))
+
+	ax1 = plt.subplot(121)
+	ax1.boxplot(data, notch=False)
+	plt.ylabel("distance in meters")
+	plt.title("Scan distance (+ ~ ahead)")
+
+	ax2 = plt.subplot(122)
+	ax2.plot(times, data, 'wo')
+	plt.xlabel("timestamp in seconds")
+	plt.ylabel("distance in meters")
+	plt.title("Scan distance (+ ~ ahead)")
+
+	plt.savefig("EtE-distance.pdf")
+
+
+def plot_cars_scanning():
+	data = {}
+
+	with open("log.carscanning.txt") as f:
+		lines = f.readlines()
+
+		for line in lines:
+			parts = line.split(" ")
+			value = float(parts[2])
+			time = round(float(parts[0]), 0)
+			if time not in data:
+				data[time] = 0
+			data[time] += 1
+
+		ax1 = plt.subplot(121)
+		ax1.boxplot(list(data.values()), notch=False)
+		plt.ylabel("#cars scanning")
+		plt.title("#cars scanning")
+
+		ax2 = plt.subplot(122)
+		ax2.plot(list(data.keys()), list(data.values()))
+		plt.xlabel("timestamp in seconds")
+		plt.ylabel("#cars scanning")
+		plt.title("#cars scanning")
+
+		plt.savefig("carscanning.pdf")
+
+
+def plot_num_cars():
+	data = []
+	times = []
+
+	with open("log.numcars.txt") as f:
+		lines = f.readlines()
+
+		for line in lines:
+			parts = line.split(" ")
+			value = float(parts[1])
+			time = float(parts[0])
+
+			data.append(value)
+			times.append(time)
+
+		ax1 = plt.subplot(121)
+		ax1.boxplot(data, notch=False)
+		plt.ylabel("# cars")
+		plt.title("# cars")
+
+		ax2 = plt.subplot(122)
+		ax2.plot(times, data)
+		plt.xlabel("timestamp in seconds")
+		plt.ylabel("# cars")
+		plt.title("# cars")
+
+		plt.savefig("cars.pdf")
+
+
+def plot_server_queue():
+	data = {}
+	times = {}
+
+	with open("log.serverqueue.txt") as f:
+		lines = f.readlines()
+
+		for line in lines:
+			parts = line.split(" ")
+			value = float(parts[2])
+			server = str(parts[1])
+			time = float(parts[0])
+
+			if server not in data:
+				data[server] = []
+				times[server] = []
+
+			data[server].append(value)
+			times[server].append(time)
+
+		ax1 = plt.subplot(121)
+		ax1.boxplot([data["server1"], data["server2"], data["server3"]], notch=False)
+		plt.xlabel("server")
+		plt.ylabel("# scans to process")
+		plt.title("server queue length")
+
+		ax2 = plt.subplot(122)
+		for server in data.keys():
+			ax2.plot(times[server], data[server], "wo")
+		plt.xlabel("timestamp in seconds")
+		plt.ylabel("# scans to process")
+		plt.title("server queue length")
+
+		plt.savefig("serverqueue.pdf")
+
+
+plot_server_queue()
+plot_num_cars()
+plot_cars_scanning()
+plot_ete_latency()
+plot_ete_distance()
