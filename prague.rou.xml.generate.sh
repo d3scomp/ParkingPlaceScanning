14b3d13a38006ee642e5c2ae197f5be1e31836f9
Vladimír Matěna <matena@d3s.mff.cuni.cz>
2017-07-11 15:56:16 +0200

    Update generation script to generate also sumo and omnet configs

--- prague.rou.xml.generate.sh
+++ prague.rou.xml.generate.sh
@@ -1,7 +1,9 @@
  #!/bin/sh
 
-template=prague.rou.xml.template
-outdir=prague.rou
+rou_template=prague.rou.xml.template
+sumo_template=prague.sumo.cfg.template
+omnet_template=omnetpp.ini.template
+sumo_port=8888
 
 if [ "$#" -ne 3 ]; then
   echo "Provide FROM and TO and STEP as params" >&2
@@ -15,11 +17,17 @@ step=${3}
 echo "Generate route configs with probability from ${from} to ${to} with step ${step} ? Enter to proceed...";
 read -r;
 
-mkdir -p ${outdir}
-
 for p in $(seq ${from} ${step} ${to}); do
-	echo ${p};
+#	echo ${p};
+	sumo_port=$((${sumo_port} + 1))
+#	echo ${sumo_port}
 	
-	file=${outdir}/prague.rou.${p}.xml
-	cat ${template} | sed "s/{PROBABILITY}/${p}/" > ${file}
+	rou_file=prague.rou.${p}.xml
+	sumo_file=prague.sumo.${p}.cfg
+	cat ${rou_template} | sed "s/{PROBABILITY}/${p}/" > ${rou_file}
+	cat ${sumo_template} | sed "s/{PROBABILITY}/${p}/" > ${sumo_file}
+	
+	echo;
+	sumo_file=$(echo "$sumo_file" | sed 's/\//\\\//')
+	cat ${omnet_template} | sed "s/{SUMO_CONFIG}/${sumo_file}/;s/{SUMO_PORT}/${sumo_port}/" > omnetpp-probability-${p}.ini
 done;
