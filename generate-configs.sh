45d233082d120971f05ebcc2a5ac1737eb928352
Vladimír Matěna <matena@d3s.mff.cuni.cz>
2018-06-13 15:15:44 +0200

    Add run counts to config generator scripts

--- generate-configs.sh
+++ generate-configs.sh
@@ -19,27 +19,27 @@ parking_step=${6}
 runs=${7}
 
 
-echo "Generate route configs with car probability from ${car_from} to ${car_to} with step ${car_step}"
+echo "Generate configs with car probability from ${car_from} to ${car_to} with step ${car_step}"
 echo "and parking probability from ${parking_from} to ${parking_to} with step ${parking_step}"
-echo "? Enter to proceed...";
+echo "and ${runs} run multiplicity ?"
+echo "Press ENTER to proceed...";
 read -r;
 
-for parking_probability in $(seq ${parking_from} ${parking_step} ${parking_to}); do
-
-	for car_probability in $(seq ${car_from} ${car_step} ${car_to}); do
-	#	echo ${p};
-		sumo_port=$((${sumo_port} + 1))
-	#	echo ${sumo_port}
-		
-		rou_file=prague.rou.${car_probability}.xml
-		sumo_file=prague.sumo.${car_probability}.cfg
-		cat ${rou_template} | sed "s/{PROBABILITY}/${car_probability}/" > ${rou_file}
-		cat ${sumo_template} | sed "s/{PROBABILITY}/${car_probability}/" > ${sumo_file}
-		
-		echo;
-		sumo_file=$(echo "$sumo_file" | sed 's/\//\\\//')
-		cat ${omnet_template} | sed "s/{SUMO_CONFIG}/${sumo_file}/;s/{SUMO_PORT}/${sumo_port}/;s/{PARKING_PROBABILITY}/${parking_probability}/" > omnetpp-car-${car_probability}-parking-${parking_probability}.ini
+for i in $(seq ${runs}); do
+	for parking_probability in $(seq ${parking_from} ${parking_step} ${parking_to}); do
+		for car_probability in $(seq ${car_from} ${car_step} ${car_to}); do
+		#	echo ${p};
+			sumo_port=$((${sumo_port} + 1))
+		#	echo ${sumo_port}
+			
+			rou_file=prague.rou.${car_probability}.xml
+			sumo_file=prague.sumo.${car_probability}.cfg
+			cat ${rou_template} | sed "s/{PROBABILITY}/${car_probability}/" > ${rou_file}
+			cat ${sumo_template} | sed "s/{PROBABILITY}/${car_probability}/" > ${sumo_file}
+			
+			echo;
+			sumo_file=$(echo "$sumo_file" | sed 's/\//\\\//')
+			cat ${omnet_template} | sed "s/{SUMO_CONFIG}/${sumo_file}/;s/{SUMO_PORT}/${sumo_port}/;s/{PARKING_PROBABILITY}/${parking_probability}/" > omnetpp-car-${car_probability}-parking-${parking_probability}-run-${i}.ini
+		done;
 	done;
-
-
 done;
