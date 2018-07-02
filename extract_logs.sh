6800f2b3fcca97d49282ab35d5a5fbbb689d06db
Vladimír Matěna <matena@d3s.mff.cuni.cz>
2018-07-02 13:22:39 +0200

    Support compressed login in extraction

--- extract_logs.sh
+++ extract_logs.sh
@@ -6,15 +6,15 @@ for log in *.logs; do
 	cd ${base}/${log};
 	echo "Extracting in $(pwd)";
 	
-	cat log.txt | grep "^# EtE lattency #" | cut -d "#" -f 3,4,5 --output-delimiter=" " | sed 's/[[:space:]]\+/ /g;s/^[[:space:]]//' > log.ete-lattency.txt
-	cat log.txt | grep "^# EtE distance #" | cut -d "#" -f 3,4,5 --output-delimiter=" " | sed 's/[[:space:]]\+/ /g;s/^[[:space:]]//' > log.ete-distance.txt
-	cat log.txt | grep "^# num cars #" | cut -d "#" -f 3,5 --output-delimiter=" " | sed 's/[[:space:]]\+/ /g;s/^[[:space:]]//' > log.numcars.txt
-	cat log.txt | grep "^# car scanning #" | cut -d "#" -f 3,4,5 --output-delimiter=" " | sed 's/[[:space:]]\+/ /g;s/^[[:space:]]//' > log.carscanning.txt
-	cat log.txt | grep "^# server processing queue #" | cut -d "#" -f 3,4,5 --output-delimiter=" " | sed 's/[[:space:]]\+/ /g;s/^[[:space:]]//' > log.serverqueue.txt
+	xzcat log.txt.xz | grep "^# EtE lattency #" | cut -d "#" -f 3,4,5 --output-delimiter=" " | sed 's/[[:space:]]\+/ /g;s/^[[:space:]]//' > log.ete-lattency.txt
+	xzcat log.txt.xz | grep "^# EtE distance #" | cut -d "#" -f 3,4,5 --output-delimiter=" " | sed 's/[[:space:]]\+/ /g;s/^[[:space:]]//' > log.ete-distance.txt
+	xzcat log.txt.xz | grep "^# num cars #" | cut -d "#" -f 3,5 --output-delimiter=" " | sed 's/[[:space:]]\+/ /g;s/^[[:space:]]//' > log.numcars.txt
+	xzcat log.txt.xz | grep "^# car scanning #" | cut -d "#" -f 3,4,5 --output-delimiter=" " | sed 's/[[:space:]]\+/ /g;s/^[[:space:]]//' > log.carscanning.txt
+	xzcat log.txt.xz | grep "^# server processing queue #" | cut -d "#" -f 3,4,5 --output-delimiter=" " | sed 's/[[:space:]]\+/ /g;s/^[[:space:]]//' > log.serverqueue.txt
 	
 done;
 
 
 # Extract simulation performance
-grep -R "simsec/sec" *.logs | sed 's/[ \t][ \t]*/ /g' | cut -d " " -f 4 | sed 's/simsec\/sec=//' > simsec-sec.txt
+#grep -R "simsec/sec" *.logs | sed 's/[ \t][ \t]*/ /g' | cut -d " " -f 4 | sed 's/simsec\/sec=//' > simsec-sec.txt
 
